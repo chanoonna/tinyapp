c23b63a9f191ff71148ae5ee6f750494ba82214e
chanoonna <chanoonna@gmail.com>
2021-01-18 20:45:27 +0000

    package: ejs

--- express_server.js
+++ express_server.js
@@ -7,6 +7,8 @@ const urlDatabase = {
   '9sm5xK': `http://www.google.com`,
 };
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.send('Hello.');
 });
@@ -17,6 +19,11 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
